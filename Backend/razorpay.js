ea59d20a7d4132b352a6f1e0e883b4f9fa63e7c5
priyanshu-pokhariya <152649792+priyanshu-pokhariya@users.noreply.github.com>
2023-12-26 22:02:36 +0530

    Delete Backend directory

--- Backend/razorpay.js
+++
@@ -1,9 +0,0 @@
-const razorpay = require("razorpay");
-require("dotenv").config({path:"./Config/config.env"});
-
-const razorpayInstance = new razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID ,
-    key_secret: process.env.RAZORPAY_KEY_SECRET
-})
-
-module.exports = razorpayInstance;
